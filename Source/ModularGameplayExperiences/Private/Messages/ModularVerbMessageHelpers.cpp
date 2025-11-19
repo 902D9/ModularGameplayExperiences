286b092df31882ab9df297cc72559da9480f0af2
Derek Reese <derek.reese@chroniclersoftware.com>
2025-11-19 14:32:19 -0800

    Adds ModularGameplay Messaging

---
+++ Source/ModularGameplayExperiences/Private/Messages/ModularVerbMessageHelpers.cpp
@@ -0,0 +1,97 @@
+﻿// Copyright Chronicler.
+
+
+#include "Messages/ModularVerbMessageHelpers.h"
+
+#include "GameFramework/Pawn.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/PlayerState.h"
+#include "GameplayEffectTypes.h"
+#include "Messages/ModularVerbMessage.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ModularVerbMessageHelpers)
+
+//////////////////////////////////////////////////////////////////////
+// FModularVerbMessage
+
+FString FModularVerbMessage::ToString() const
+{
+	FString HumanReadableMessage;
+	FModularVerbMessage::StaticStruct()->ExportText(/*out*/ HumanReadableMessage, this, /*Defaults=*/ nullptr, /*OwnerObject=*/ nullptr, PPF_None, /*ExportRootScope=*/ nullptr);
+	return HumanReadableMessage;
+}
+
+//////////////////////////////////////////////////////////////////////
+// 
+
+APlayerState* UModularVerbMessageHelpers::GetPlayerStateFromObject(UObject* Object)
+{
+	if (APlayerController* PC = Cast<APlayerController>(Object))
+	{
+		return PC->PlayerState;
+	}
+
+	if (APlayerState* TargetPS = Cast<APlayerState>(Object))
+	{
+		return TargetPS;
+	}
+	
+	if (APawn* TargetPawn = Cast<APawn>(Object))
+	{
+		if (APlayerState* TargetPS = TargetPawn->GetPlayerState())
+		{
+			return TargetPS;
+		}
+	}
+	return nullptr;
+}
+
+APlayerController* UModularVerbMessageHelpers::GetPlayerControllerFromObject(UObject* Object)
+{
+	if (APlayerController* PC = Cast<APlayerController>(Object))
+	{
+		return PC;
+	}
+
+	if (APlayerState* TargetPS = Cast<APlayerState>(Object))
+	{
+		return TargetPS->GetPlayerController();
+	}
+
+	if (APawn* TargetPawn = Cast<APawn>(Object))
+	{
+		return Cast<APlayerController>(TargetPawn->GetController());
+	}
+
+	return nullptr;
+}
+
+FGameplayCueParameters UModularVerbMessageHelpers::VerbMessageToCueParameters(const FModularVerbMessage& Message)
+{
+	FGameplayCueParameters Result;
+
+	Result.OriginalTag = Message.Verb;
+	Result.Instigator = Cast<AActor>(Message.Instigator);
+	Result.EffectCauser = Cast<AActor>(Message.Target);
+	Result.AggregatedSourceTags = Message.InstigatorTags;
+	Result.AggregatedTargetTags = Message.TargetTags;
+	//@TODO: = Message.ContextTags;
+	Result.RawMagnitude = Message.Magnitude;
+
+	return Result;
+}
+
+FModularVerbMessage UModularVerbMessageHelpers::CueParametersToVerbMessage(const FGameplayCueParameters& Params)
+{
+	FModularVerbMessage Result;
+	
+	Result.Verb = Params.OriginalTag;
+	Result.Instigator = Params.Instigator.Get();
+	Result.Target = Params.EffectCauser.Get();
+	Result.InstigatorTags = Params.AggregatedSourceTags;
+	Result.TargetTags = Params.AggregatedTargetTags;
+	//@TODO: Result.ContextTags = ???;
+	Result.Magnitude = Params.RawMagnitude;
+
+	return Result;
+}
---
+++ Source/ModularGameplayExperiences/Public/Messages/ModularVerbMessageHelpers.h
@@ -0,0 +1,37 @@
+﻿// Copyright Chronicler.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ModularVerbMessageHelpers.generated.h"
+
+struct FGameplayCueParameters;
+struct FModularVerbMessage;
+
+class APlayerController;
+class APlayerState;
+class UObject;
+struct FFrame;
+/**
+ * 
+ */
+UCLASS()
+class MODULARGAMEPLAYEXPERIENCES_API UModularVerbMessageHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintCallable)
+	static APlayerState* GetPlayerStateFromObject(UObject* Object);
+
+	UFUNCTION(BlueprintCallable)
+	static APlayerController* GetPlayerControllerFromObject(UObject* Object);
+
+	UFUNCTION(BlueprintCallable)
+	static FGameplayCueParameters VerbMessageToCueParameters(const FModularVerbMessage& Message);
+
+	UFUNCTION(BlueprintCallable)
+	static FModularVerbMessage CueParametersToVerbMessage(const FGameplayCueParameters& Params);
+};
